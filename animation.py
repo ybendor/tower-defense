05a5562ffe112d112a098102ee11c1704fd31a3a
yoav1994 <49597056+yoav1994@users.noreply.github.com>
2019-04-15 10:56:19 +0300

    class declaration change, syntax and minor bug fixes (#4)
    * Game kind of works
    
    * Add first CI sketch
    
    * Correcting flake8 errors
    
    tried to correct most of the errors that flake8 mentioned
    
    * Fixed flake8 errors
    
    Fixed flake8 errors

--- animation.py
+++ animation.py
@@ -1,4 +1,5 @@
-from Tkinter import *
+from tkinter import (Tk, Canvas, ALL)
+
 
 ###########################################
 # Animation class
@@ -7,42 +8,50 @@ from Tkinter import *
 class Animation(object):
     # Override these methods when creating an animation
     def mousePressed(self, event): pass
+
     def keyPressed(self, event): pass
+
     def timerFired(self): pass
+
     def init(self): pass
+
     def redrawAll(self): pass
-    
+
     def run(self, width=1000, height=600):
-		# create the root and the canvas
-		root = Tk()
-		self.width = width
-		self.height = height
-		self.boardDim = self.width-400
-		self.canvas = Canvas(root, width=width, height=height)
-		self.canvas.pack()	
-		# set up events
-		def redrawAllWrapper():
-			self.canvas.delete(ALL)
-			self.redrawAll()
-		def mousePressedWrapper(event):
-			self.mousePressed(event)
-			redrawAllWrapper()
-		def keyPressedWrapper(event):
-			self.keyPressed(event)
-			redrawAllWrapper()
-		root.bind("<Button-1>", mousePressedWrapper)
-		root.bind("<Key>", keyPressedWrapper)
-		# set up timerFired events
-		self.timerFiredDelay = 50 
-		def timerFiredWrapper():
-			self.timerFired()
-			redrawAllWrapper()
-			# pause, then call timerFired again
-			self.canvas.after(self.timerFiredDelay, timerFiredWrapper)
-		# init and get timerFired running
-		self.init()
-		timerFiredWrapper()
-		# launch the app
-		root.mainloop() 
+        # create the root and the canvas
+        root = Tk()
+        self.width = width
+        self.height = height
+        self.boardDim = self.width - 400
+        self.canvas = Canvas(root, width=width, height=height)
+        self.canvas.pack()
 
+        # set up events
+        def redrawAllWrapper():
+            self.canvas.delete(ALL)
+            self.redrawAll()
 
+        def mousePressedWrapper(event):
+            self.mousePressed(event)
+            redrawAllWrapper()
+
+        def keyPressedWrapper(event):
+            self.keyPressed(event)
+            redrawAllWrapper()
+
+        root.bind("<Button-1>", mousePressedWrapper)
+        root.bind("<Key>", keyPressedWrapper)
+        # set up timerFired events
+        self.timerFiredDelay = 50
+
+        def timerFiredWrapper():
+            self.timerFired()
+            redrawAllWrapper()
+            # pause, then call timerFired again
+            self.canvas.after(self.timerFiredDelay, timerFiredWrapper)
+
+        # init and get timerFired running
+        self.init()
+        timerFiredWrapper()
+        # launch the app
+        root.mainloop()
--- tower.py
+++ tower.py
@@ -2,75 +2,82 @@
 # Tower Array class
 ###########################################
 
-class TowerArray(object):
-	def __init__(self):
-		self.towerList = []
+from extras import Shot
+
+
+class TowerArray:
+    def __init__(self):
+        self.towerList = []
 
 
 ###########################################
 # Tower class
 ###########################################
 
-class Tower(object):
-	def __init__(self, row, col, board, cellDim):
-		self.row = row
-		self.col = col
-		self.board = board
-		self.cellDim = cellDim
-		self.location = self.calculateLocation(
-		self.row, self.col, cellDim)
-		self.shotOnScreen = False
-		self.radius = 70
-		self.center = self.calculateCenter(self.location) 
-		self.shots = []
-		self.color = "black"
-		self.shotSpeed = 1.4
-		self.shotDamage = 0
-		self.slowDown = False
+class Tower:
+    def __init__(self, row, col, board, cellDim):
+        self.row = row
+        self.col = col
+        self.board = board
+        self.cellDim = cellDim
+        self.location = self.calculateLocation(
+            self.row, self.col, cellDim)
+        self.shotOnScreen = False
+        self.radius = 70
+        self.center = self.calculateCenter(self.location)
+        self.shots = []
+        self.color = "black"
+        self.shotSpeed = 1.4
+        self.shotDamage = 0
+        self.slowDown = False
 
-	def __repr__(self):
-		return "Tower(%r, %r, %r)" % (self.row, self.col, self.color)
+    def __repr__(self):
+        return "Tower(%r, %r, %r)" % (self.row, self.col, self.color)
 
-	def calculateLocation(self, row, col, cellDim):	
-		startx = col*cellDim
-		starty = row*cellDim
-		endx = startx + cellDim
-		endy = starty + cellDim
-		return [startx, starty, endx, endy]
+    def calculateLocation(self, row, col, cellDim):
+        startx = col * cellDim
+        starty = row * cellDim
+        endx = startx + cellDim
+        endy = starty + cellDim
+        return [startx, starty, endx, endy]
 
-	def calculateCenter(self, location): 
-		centerX = (location[2] - location[0])/2.0 + location[0] 
-		centerY = (location[3] - location[1])/2.0 + location[1]
-		return [centerX, centerY]
+    def calculateCenter(self, location):
+        centerX = (location[2] - location[0]) / 2.0 + location[0]
+        centerY = (location[3] - location[1]) / 2.0 + location[1]
+        return [centerX, centerY]
 
-	def fireShot(self, enemy):
-		self.shotOnScreen = True
-		shot = Shot(self, enemy, self.board, self.cellDim)
-		self.shots.append(shot)   
-			
+    def fireShot(self, enemy):
+        self.shotOnScreen = True
+        shot = Shot(self, enemy, self.board, self.cellDim)
+        self.shots.append(shot)
 
-###########################################
+    def get_color(self):
+        return self.color
+
+    ###########################################
+
+
 # Orange Tower class
 ###########################################
 
 class OrangeTower(Tower):
-	def __init__(self, row, col, board, cellDim):
-		super(OrangeTower, self).__init__(row, col, board, cellDim)
-		self.color = "orange"
-		self.cost = 3
-		self.shotDamage = 1
-	
-		
+    def __init__(self, row, col, board, cellDim):
+        super().__init__(row, col, board, cellDim)
+        self.color = "orange"
+        self.cost = 3
+        self.shotDamage = 1
+
+
 ###########################################
 # Red Tower class
 ###########################################
 
 class RedTower(Tower):
-	def __init__(self, row, col, board, cellDim):
-		super(RedTower, self).__init__(row, col, board, cellDim)
-		self.color = "red"
-		self.cost = 10
-		self.shotDamage = 2
+    def __init__(self, row, col, board, cellDim):
+        super().__init__(row, col, board, cellDim)
+        self.color = "red"
+        self.cost = 10
+        self.shotDamage = 2
 
 
 ###########################################
@@ -78,13 +85,13 @@ class RedTower(Tower):
 ###########################################
 
 class GreenTower(Tower):
-	def __init__(self, row, col, board, cellDim):
-		super(GreenTower, self).__init__(row, col, board, cellDim)
-		self.color = "green"
-		self.cost = 15
-		self.shotSpeed = 1.6
-		self.radius = 90
-		self.shotDamage = 3
+    def __init__(self, row, col, board, cellDim):
+        super().__init__(row, col, board, cellDim)
+        self.color = "green"
+        self.cost = 15
+        self.shotSpeed = 1.6
+        self.radius = 90
+        self.shotDamage = 3
 
 
 ###########################################
@@ -92,11 +99,9 @@ class GreenTower(Tower):
 ###########################################
 
 class PurpleTower(Tower):
-	def __init__(self, row, col, board, cellDim):
-		super(PurpleTower, self).__init__(row, col, board, cellDim)
-		self.color = "#8C489F"
-		self.radius = 65 
-		self.slowDown = True
-		self.cost = 20
-
-
+    def __init__(self, row, col, board, cellDim):
+        super().__init__(row, col, board, cellDim)
+        self.color = "#8C489F"
+        self.radius = 65
+        self.slowDown = True
+        self.cost = 20
